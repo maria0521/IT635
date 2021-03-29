61c9358bbf00e6fcb42e723127078354fcae5a79
maria0521 <55172246+maria0521@users.noreply.github.com>
2021-03-29 14:46:46 -0400

    Create TotalUserSpent.py

---
+++ TotalUserSpent.py
@@ -0,0 +1,18 @@
+
+import psycopg2
+
+conn = psycopg2.connect("postgres://decor_manager:testing@localhost:5432/wallartdecor")
+cur = conn.cursor()
+
+while True:
+  print("Please enter Username")
+  username = str(input())
+ 
+ 
+  cur.execute("SELECT SUM(Price) FROM TRANSACTIONS WHERE Username = '%s'");
+  TotalPrice = cur.fetchall()
+  print("%s has spent a total of: ${TotalPrice}")
+  conn.commit()
+  
+cur.close()
+conn.close()
