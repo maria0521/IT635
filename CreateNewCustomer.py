a681638da28ac4020a3a2a2f32d38babd17eda9d
maria0521 <55172246+maria0521@users.noreply.github.com>
2021-03-29 14:13:20 -0400

    Update CreateNewCustomer.py

--- CreateNewCustomer.py
+++ CreateNewCustomer.py
@@ -21,11 +21,11 @@ while True:
   print("Please enter Zipcode")
   zipcode = int(input())
  
-    cur.execute("""
-        INSERT INTO CUSTOMER (Username, Password, CName, BillingAddress, Str1, City, State, Zip)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
-    """, (username, passw, cname, billadd, mailadd, city, state, zipcode));
-    conn.commit()
+  cur.execute("""
+    INSERT INTO CUSTOMER (Username, Password, CName, BillingAddress, Str1, City, State, Zip)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+  """, (username, passw, cname, billadd, mailadd, city, state, zipcode));
+  conn.commit()
   print("record inserted")
 cur.close()
 conn.close()
