9c2cdd4cfbfb153de9b8b1c36c600ca15fc5348f
maria0521 <55172246+maria0521@users.noreply.github.com>
2021-03-29 16:11:45 -0400

    Update UserTransHistory.py

--- UserTransHistory.py
+++ UserTransHistory.py
@@ -7,8 +7,8 @@ print("Please enter Username")
 Username = str(input())
 cur.execute("SELECT * FROM transactions WHERE Username = '%s'");
 result = cur.fetchall()
-for x in result:
-    print(x)
+for row in result:
+    print(row[0])
 conn.commit()
 
 cur.close()
