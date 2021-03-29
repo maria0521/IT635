adf0b8a6998bf3b8c0e4dd3fdb9de55f1216bbaa
maria0521 <55172246+maria0521@users.noreply.github.com>
2021-03-29 14:30:54 -0400

    Update UserTransHistory.py

--- UserTransHistory.py
+++ UserTransHistory.py
@@ -3,14 +3,13 @@ import psycopg2
 conn = psycopg2.connect("postgres://decor_manager:testing@localhost:5432/wallartdecor")
 cur = conn.cursor()
 
-while True:
-    print("Please enter Username")
-    Username = str(input())
-    cur.execute("SELECT * FROM transactions WHERE Username = %s");
-    result = cur.fetchall()
-    for x in result:
-     print(x)
-    conn.commit()
+print("Please enter Username")
+Username = str(input())
+cur.execute("SELECT * FROM transactions WHERE Username = %s");
+result = cur.fetchall()
+for x in result:
+    print(x)
+conn.commit()
 
 cur.close()
 conn.close()
